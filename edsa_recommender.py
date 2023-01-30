27bbc309e6d81fbb13d7dc2f7742cc6b3b6d4c1b
Aniedi Oboho-Etuk <112181312+Oboho-Etuk@users.noreply.github.com>
2023-01-30 11:39:19 +0100

    Update edsa_recommender.py

--- edsa_recommender.py
+++ edsa_recommender.py
@@ -108,8 +108,8 @@ def main():
 	if sys == 'Collaborative Based Filtering':
 		if st.button("Recommend"):
 			try:
-                    		with st.spinner('Crunching the numbers...'):
-                        		top_recommendations = collab_model(movie_list=fav_movies, top_n=10)
+				with st.spinner('Crunching the numbers...'):
+					top_recommendations = collab_model(movie_list=fav_movies, top_n=10)
 					st.title("We think you'll like:")
 					for i,j in enumerate(top_recommendations):
 						st.subheader(str(i+1)+'. '+j)
