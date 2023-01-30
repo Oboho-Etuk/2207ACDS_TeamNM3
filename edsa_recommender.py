ff4aaf0bc72c50a37e153e98e77e74b606251691
Aniedi Oboho-Etuk <112181312+Oboho-Etuk@users.noreply.github.com>
2023-01-30 11:20:43 +0100

    Update edsa_recommender.py

--- edsa_recommender.py
+++ edsa_recommender.py
@@ -109,8 +109,7 @@ def main():
 		if st.button("Recommend"):
 			try:
                     		with st.spinner('Crunching the numbers...'):
-                        		top_recommendations = collab_model(movie_list=fav_movies,
-                                                           top_n=10)
+                        		top_recommendations = collab_model(movie_list=fav_movies, top_n=10)
 					st.title("We think you'll like:")
 					for i,j in enumerate(top_recommendations):
 						st.subheader(str(i+1)+'. '+j)
