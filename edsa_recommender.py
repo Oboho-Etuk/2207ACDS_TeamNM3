cdb09a8e4423cd2f232351559e280d56b527cb35
Aniedi Oboho-Etuk <112181312+Oboho-Etuk@users.noreply.github.com>
2023-01-30 11:16:56 +0100

    Update edsa_recommender.py

--- edsa_recommender.py
+++ edsa_recommender.py
@@ -111,11 +111,11 @@ def main():
                     		with st.spinner('Crunching the numbers...'):
                         		top_recommendations = collab_model(movie_list=fav_movies,
                                                            top_n=10)
-                    			st.title("We think you'll like:")
-                    			for i,j in enumerate(top_recommendations):
-                        			st.subheader(str(i+1)+'. '+j)
+					st.title("We think you'll like:")
+					for i,j in enumerate(top_recommendations):
+						st.subheader(str(i+1)+'. '+j)
 			except:
-                    		st.error("Oops! Looks like this algorithm does't work.\
+				st.error("Oops! Looks like this algorithm does't work.\
                               	We'll need to fix it!")
 				
 				
