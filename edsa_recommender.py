45852ce54b7bbb43203493474f05ef210d8fa45b
Aniedi Oboho-Etuk <112181312+Oboho-Etuk@users.noreply.github.com>
2023-01-30 10:38:33 +0100

    Update edsa_recommender.py

--- edsa_recommender.py
+++ edsa_recommender.py
@@ -42,6 +42,7 @@ title_list = load_movie_titles('resources/data/movies.csv')
 
 # App declaration
 def main():
+	
 	st.info("Team Information - Experts and their Roles")
 	# You can read a markdown file from supporting resources folder
 	st.markdown("This Web app has been adapted and developed by the Infinity AI, a                              \
@@ -51,7 +52,7 @@ def main():
 	providing exceptional services, and using excellent professional expertise.                            \n\
 	We help businesses get valuable insights into data, automate repetitive tasks,                          \
 	enhance performance, add AI-driven features, and prevent cost overruns.")
-
+	
 	st.subheader("Meet the Team")
 	if st.button('Aniedi'): # information is hidden if button is clicked
 		st.markdown('Aniedi Oboho-Etuk is a Infinity AI Developer')
@@ -65,17 +66,17 @@ def main():
 		st.markdown('Tshepiso Padi is the Infinity AI Product Owner')
     	if st.button('Ndinnanyi'): # information is hidden if button is clicked
 		st.markdown('Ndinanyi Justice is the Infinity AI Brand Developer')
-
-    # DO NOT REMOVE the 'Recommender System' option below, however,
-    # you are welcome to add more options to enrich your app.
+	
+    	# DO NOT REMOVE the 'Recommender System' option below, however,
+    	# you are welcome to add more options to enrich your app.
 	page_options = ["Recommender System","Solution Overview"]
-
+	
     	# -------------------------------------------------------------------
     	# ----------- !! THIS CODE MUST NOT BE ALTERED !! -------------------
     	# -------------------------------------------------------------------
     	page_selection = st.sidebar.selectbox("Choose Option", page_options)
     	if page_selection == "Recommender System":
-        	# Header contents
+		# Header contents
         	st.write('# Movie Recommender Engine')
         	st.write('### EXPLORE Data Science Academy Unsupervised Predict')
         	st.image('resources/imgs/Image_header.png',use_column_width=True)
@@ -83,52 +84,51 @@ def main():
         	sys = st.radio("Select an algorithm",
                        ('Content Based Filtering',
                         'Collaborative Based Filtering'))
-
+	
         	# User-based preferences
         	st.write('### Enter Your Three Favorite Movies')
         	movie_1 = st.selectbox('Fisrt Option',title_list[14930:15200])
         	movie_2 = st.selectbox('Second Option',title_list[25055:25255])
         	movie_3 = st.selectbox('Third Option',title_list[21100:21200])
         	fav_movies = [movie_1,movie_2,movie_3]
-
+		
         # Perform top-10 movie recommendation generation
         if sys == 'Content Based Filtering':
-            if st.button("Recommend"):
-                try:
-                    with st.spinner('Crunching the numbers...'):
-                        top_recommendations = content_model(movie_list=fav_movies,
+		if st.button("Recommend"):
+			try:
+				with st.spinner('Crunching the numbers...'):
+					top_recommendations = content_model(movie_list=fav_movies,
                                                             top_n=10)
-                    st.title("We think you'll like:")
-                    for i,j in enumerate(top_recommendations):
-                        st.subheader(str(i+1)+'. '+j)
-                except:
-                    st.error("Oops! Looks like this algorithm does't work.\
-                              We'll need to fix it!")
-
-
-        if sys == 'Collaborative Based Filtering':
-            if st.button("Recommend"):
-                try:
-                    with st.spinner('Crunching the numbers...'):
-                        top_recommendations = collab_model(movie_list=fav_movies,
+					st.title("We think you'll like:")
+					for i,j in enumerate(top_recommendations):
+						st.subheader(str(i+1)+'. '+j)
+                	except:
+                    		st.error("Oops! Looks like this algorithm does't work.\
+				We'll need to fix it!")
+        
+	if sys == 'Collaborative Based Filtering':
+		if st.button("Recommend"):
+                	try:
+                    		with st.spinner('Crunching the numbers...'):
+                        		top_recommendations = collab_model(movie_list=fav_movies,
                                                            top_n=10)
-                    st.title("We think you'll like:")
-                    for i,j in enumerate(top_recommendations):
-                        st.subheader(str(i+1)+'. '+j)
-                except:
-                    st.error("Oops! Looks like this algorithm does't work.\
-                              We'll need to fix it!")
+                    			st.title("We think you'll like:")
+                    			for i,j in enumerate(top_recommendations):
+                        			st.subheader(str(i+1)+'. '+j)
+                	except:
+                    		st.error("Oops! Looks like this algorithm does't work.\
+                              	We'll need to fix it!")
+				
+				
+    	# -------------------------------------------------------------------
+	
+    	# ------------- SAFE FOR ALTERING/EXTENSION -------------------
+    	if page_selection == "Solution Overview":
+        	st.title("Solution Overview")
+        	st.write("Describe your winning approach on this page")
 
-
-    # -------------------------------------------------------------------
-
-    # ------------- SAFE FOR ALTERING/EXTENSION -------------------
-    if page_selection == "Solution Overview":
-        st.title("Solution Overview")
-        st.write("Describe your winning approach on this page")
-
-    # You may want to add more sections here for aspects such as an EDA,
-    # or to provide your business pitch.
+    	# You may want to add more sections here for aspects such as an EDA,
+    	# or to provide your business pitch.
 
 
 if __name__ == '__main__':
