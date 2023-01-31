42498d4e3d442691edd0efd39a7fff49dee41571
Aniedi Oboho-Etuk <112181312+Oboho-Etuk@users.noreply.github.com>
2023-01-31 12:51:22 +0100

    Update edsa_recommender.py

--- edsa_recommender.py
+++ edsa_recommender.py
@@ -36,13 +36,13 @@ def main():
 
     # DO NOT REMOVE the 'Recommender System' option below, however,
     # you are welcome to add more options to enrich your app.
-    page_options = ["Recommender System","Solution Overview", "About The Team"]
-
+	page_options = ["Recommender System","Solution Overview", "About The Team"]
+	
     # -------------------------------------------------------------------
     # ----------- !! THIS CODE MUST NOT BE ALTERED !! -------------------
     # -------------------------------------------------------------------
-    page_selection = st.sidebar.selectbox("Choose Option", page_options)
-    if page_selection == "Recommender System":
+	page_selection = st.sidebar.selectbox("Choose Option", page_options)
+	if page_selection == "Recommender System":
         # Header contents
         st.write('# Movie Recommender Engine')
         st.write('### EXPLORE Data Science Academy Unsupervised Predict')
@@ -51,8 +51,8 @@ def main():
         sys = st.radio("Select an algorithm",
                        ('Content Based Filtering',
                         'Collaborative Based Filtering'))
-
-        # User-based preferences
+		
+		# User-based preferences
         st.write('### Enter Your Three Favorite Movies')
         movie_1 = st.selectbox('Fisrt Option',title_list[14930:15200])
         movie_2 = st.selectbox('Second Option',title_list[25055:25255])
@@ -91,9 +91,9 @@ def main():
     # -------------------------------------------------------------------
 
     # ------------- SAFE FOR ALTERING/EXTENSION -------------------
-    if page_selection == "Solution Overview":
-        st.title("Solution Overview")
-        st.markdown("Around the world, movie industries have been blessed with creative geniuses in the form of directors,   \
+		if page_selection == "Solution Overview":
+			st.title("Solution Overview")
+			st.markdown("Around the world, movie industries have been blessed with creative geniuses in the form of directors,   \
             screenwriters, actors, sound designers and cinematographers. Together with the rise in popularity of portable    \
             devices, capable of hosting streaming services, movies have ensured that people can stay glued to their          \
             favourites whether in transit or in the corners of their homes. \
@@ -107,85 +107,84 @@ def main():
             In today’s technology driven world, there have been several attempts to solving this problem using recommender \
             systems. These systems are basically a subclass of intelligent information filtering processes that provide    \
             suggestions for items that are most pertinent to a particular user.")
-        
-        st.subheader("Approach")
-        st.markdown("In this project, the **Infinity AI** team identifies some insights into data that can be used for the \
-        development of a few recommender systems. The team explores eight datasets of more than 48000 movies and over   \
-        160000 users with up to 15 million of datapoints containing movie ratings, genres, keywords, and so on collected   \
-        from Explore Ai Academy (EDSA) and the MovieLens datasets. Using these datasets, the team attempts to answer       \
-        various questions about movies. Delivering an accurate and robust solution to this challenge has immense economic  \
-        potential for industry clients, with users of the system being exposed to content they would like to view or       \
-        purchase - generating revenue and platform affinity.")
+			st.subheader("Approach")
+			st.markdown("In this project, the **Infinity AI** team identifies some insights into data that can be used for the \
+			development of a few recommender systems. The team explores eight datasets of more than 48000 movies and over   \
+			160000 users with up to 15 million of datapoints containing movie ratings, genres, keywords, and so on collected   \
+			from Explore Ai Academy (EDSA) and the MovieLens datasets. Using these datasets, the team attempts to answer       \
+			various questions about movies. Delivering an accurate and robust solution to this challenge has immense economic  \
+			potential for industry clients, with users of the system being exposed to content they would like to view or       \
+			purchase - generating revenue and platform affinity.")
     # You may want to add more sections here for aspects such as an EDA,
     # or to provide your business pitch.
-    
-    if page_selection == "About The Team":
-        st.title("About The Team")
+	
+		if page_selection == "About The Team":
+			st.title("About The Team")
 
-        st.markdown("Infinity AI is a leading data science firm and AI-powered solutions provider with its own R&D Center. \
-        \
-        \
-        Infinity AI’s mission is to help clients improve competitiveness and get great results from deploying tools in their \
-        businesses. The team strives to reach these goals by applying innovative and proprietary development technologies,   \
-        providing exceptional services, and using excellent professional expertise.                                          \
-        \
-        \
-        Infinity AI is reputed for helping businesses get valuable insights from data, support customer retention while      \
-        minimizing churn, optimize earnings, automate repetitive tasks, enhance performance, add AI-driven features, and     \
-        prevent cost overruns.")
+			st.markdown("Infinity AI is a leading data science firm and AI-powered solutions provider with its own R&D Center. \
+			\
+			\
+			Infinity AI’s mission is to help clients improve competitiveness and get great results from deploying tools in their \
+			businesses. The team strives to reach these goals by applying innovative and proprietary development technologies,   \
+			providing exceptional services, and using excellent professional expertise.                                          \
+			\
+			\
+			Infinity AI is reputed for helping businesses get valuable insights from data, support customer retention while      \
+			minimizing churn, optimize earnings, automate repetitive tasks, enhance performance, add AI-driven features, and     \
+			prevent cost overruns.")
 
-        st.write("Roles and Responsibilities of the Team")
-        st.subheader("Meet the Team")
-        if st.button('Bongani'): # information is hidden if button is clicked
-            st.markdown('Bongani Mavuso is a Infinity AI CEO')
-        if st.button('Aniedi'): # information is hidden if button is clicked
-            st.markdown('Aniedi Oboho-Etuk is the Infinity AI Developer')
-        if st.button('Tshepiso'): # information is hidden if button is clicked
-            st.markdown('Tshepiso Padi is a Infinity Project Manager')
-        if st.button('Josiah'): # information is hidden if button is clicked
-            st.markdown('Josiah Aramide is the Infinity AI Developer/Strategist')
-        if st.button('Manoko'): # information is hidden if button is clicked
-            st.markdown('Manoko Langa is the Infinity AI Communications Manager')
-        if st.button('Justice'): # information is hidden if button is clicked
-            st.markdown('Ndinnanyi Justice is the Infinity AI Sales Manager')
-        if st.button('Nsika'): # information is hidden if button is clicked
-            st.markdown('Nsika Masondo is the Infinity AI Quality Control Manager')
+			st.write("Roles and Responsibilities of the Team")
+			st.subheader("Meet the Team")
+			if st.button('Bongani'): # information is hidden if button is clicked
+				st.markdown('Bongani Mavuso is a Infinity AI CEO')
+			if st.button('Aniedi'): # information is hidden if button is clicked
+				st.markdown('Aniedi Oboho-Etuk is the Infinity AI Developer')
+			if st.button('Tshepiso'): # information is hidden if button is clicked
+				st.markdown('Tshepiso Padi is a Infinity Project Manager')
+			if st.button('Josiah'): # information is hidden if button is clicked
+				st.markdown('Josiah Aramide is the Infinity AI Developer/Strategist')
+			if st.button('Manoko'): # information is hidden if button is clicked
+				st.markdown('Manoko Langa is the Infinity AI Communications Manager')
+			if st.button('Justice'): # information is hidden if button is clicked
+				st.markdown('Ndinnanyi Justice is the Infinity AI Sales Manager')
+			if st.button('Nsika'): # information is hidden if button is clicked
+				st.markdown('Nsika Masondo is the Infinity AI Quality Control Manager')
 	# -------------------------------------------------------------------
 	
 	# ------------- SAFE FOR ALTERING/EXTENSION -------------------------
 	
-    if page_selection == "Solution Overview":
-	st.title("Solution Overview")
-	st.write("Describe your winning approach on this page")
+		if page_selection == "Solution Overview":
+			st.title("Solution Overview")
+		st.write("Describe your winning approach on this page")
 
-	# Select Movies
-	movie_list = movies['title'].values
+		# Select Movies
+		movie_list = movies['title'].values
 
-	selected_movie = st.selectbox( "Type or select a movie from the dropdown", movie_list )
+		selected_movie = st.selectbox( "Type or select a movie from the dropdown", movie_list )
 
-	if st.button('Show Recommendation'):
-		recommended_movie_names = get_recommendations(selected_movie)
-		recommended_movie_names
+		if st.button('Show Recommendation'):
+			recommended_movie_names = get_recommendations(selected_movie)
+			recommended_movie_names
 
 
-		# Plotly Table
-	def table(df):
-		fig=go.Figure(go.table( columnorder = [1,2,3],
-						   columnwidth = [10,28],
-						   header=dict(values=[' title','genres'],
-							   line_color='black',font=dict(color='black',size= 19),height=40,
-							   fill_color='#dd571c',#
-							   align=['left','center']),
-						   cells=dict(values=[movies.title,movies.genres],
-							  fill_color='#ffdac4',line_color='grey',
-							  font=dict(color='black', family="Lato", size=16),
-							  align='left')))
-		fig.update_layout(height=600, title ={'text': "Top 10 Movie Recommendations", 'font': {'size': 22}},title_x=0.5)
-		return fig.show()
+			# Plotly Table
+		def table(df):
+			fig=go.Figure(go.table( columnorder = [1,2,3],
+							   columnwidth = [10,28],
+							   header=dict(values=[' title','genres'],
+								   line_color='black',font=dict(color='black',size= 19),height=40,
+								   fill_color='#dd571c',#
+								   align=['left','center']),
+							   cells=dict(values=[movies.title,movies.genres],
+								  fill_color='#ffdac4',line_color='grey',
+								  font=dict(color='black', family="Lato", size=16),
+								  align='left')))
+			fig.update_layout(height=600, title ={'text': "Top 10 Movie Recommendations", 'font': {'size': 22}},title_x=0.5)
+			return fig.show()
 
-	if st.button('Show Recommendation'):
-		recommended_movie_names = get_recommendations(selected_movie)
-		table(recommended_movie_names)
+		if st.button('Show Recommendation'):
+			recommended_movie_names = get_recommendations(selected_movie)
+			table(recommended_movie_names)
 	
 	
 	# You may want to add more sections here for aspects such as an EDA,
